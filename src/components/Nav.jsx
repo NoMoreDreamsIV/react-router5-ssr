455ea3382af9571100ef512338ce7bbff0930f88
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-04-15 10:07:34 +0400

    найден забытый return и отсутствующий роутинг routeName="home" заменен на существующий home

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,8 @@
 import { ConnectedLink } from "react-router5";
 
-export default function Nav() {
-    <nav>
-        <ConnectedLink routeName="name">Home</ConnectedLink> 
+export default function Nav() { // :)
+    return <nav>   
+        <ConnectedLink routeName="home">Home</ConnectedLink> 
         <ConnectedLink routeName="about">About</ConnectedLink> 
         <ConnectedLink routeName="info">Info</ConnectedLink> 
     </nav>
